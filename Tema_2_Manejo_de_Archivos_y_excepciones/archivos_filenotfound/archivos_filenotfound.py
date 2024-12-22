545c839704d242e85336dccfff650c781317b100
Antonio Zurano <azurano@outlook.es>
2024-12-22 18:56:28 +0100

    Ejercicio Archivos_filenotfound.py

---
+++ Tema_2_Manejo_de_Archivos_y_excepciones/archivos_filenotfound/archivos_filenotfound.py
@@ -0,0 +1,21 @@
+# Comporbar que nuestros archivos estan disponibles
+# Si lo estan imprimimos el contenido de los archivos
+
+
+filenames = ["gatos.txt", "perros.txt"]
+
+for filename in filenames:
+    try:    
+        with open(filename, "r") as file:
+            print(f"Contenido del {filename}:")
+            contenido = file.read()
+            print(contenido)
+    except FileNotFoundError:
+        print(f"El archivo {filename} no se encuentra disponible.")
+        print("\n")
+    # cualquier otro error que no sea FileNotFoundError
+    except:  
+        pass
+    # otra forma de manejar el error
+    #    print(f"El archivo {filename} no se encuentra disponible.")
+    #    print("\n")
